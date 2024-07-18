0d17a6c49b36aa145f4f5399237ae00c4ab6fb5a
mahadeshwara-prasad <120275936+mahadeshwara-prasad@users.noreply.github.com>
2024-07-18 12:11:17 +0530

    Update ethers.js

--- ethereum/ethers.js
+++ ethereum/ethers.js
@@ -6,10 +6,10 @@ async function getProvider(){
     if(typeof window == null && typeof window.ethereum == null) {
 
         console.log("MetaMask not installed; using read-only defaults")
-        provider = ethers.getDefaultProvider("https://sepolia.infura.io/v3/52b0e6f5c3c546f0a92fe3adb34f21c4");
+        provider = ethers.getDefaultProvider("Enter your provider here");
         signer = await provider.getSigner();
     } else {
-        //provider = ethers.getDefaultProvider("https://sepolia.infura.io/v3/52b0e6f5c3c546f0a92fe3adb34f21c4");
+        
         provider = new ethers.BrowserProvider(window.ethereum);
         signer = await provider.getSigner();
         
